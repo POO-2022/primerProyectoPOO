4d0e6aa0a1e8edb1dcba94d4291b3ab5961ea3a6
Andy Porras <andyporras@estudiantec.cr>
2022-04-17 22:27:38 -0600

    primera mentana

--- Gestor de Solicitudes de Crédito/interfaz/ventana.java
+++ Gestor de Solicitudes de Crédito/interfaz/ventana.java
@@ -9,18 +9,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class ventana extends JFrame {
-  private JButton boton1;
-  private JButton boton2;
-  private JButton boton3;
-  private JButton boton4;
-  private JButton boton5;
-  private JButton boton6;
-  private JButton boton7;
-  private JButton boton8;
-  private JButton boton9;
-  private JButton boton10;
-  private JButton boton11;
-  private JButton boton12;
+  private JButton registrarSolicitante;
+  private JButton registrarSolicitudCredito;
+  private JButton calcularCreditoHipotecarioEnColon;
+  private JButton calcularCreditoHipotecarioDolar;
+  private JButton calculoCreditoFiduciarioColon;
+  private JButton calculoCreditoFiduciarioDolar;
+  private JButton calculoCreditoPrendarioColor;
+  private JButton calculoCreditoPrendarioDolar;
+  private JButton calculoCreditoPersonalColor;
+  private JButton calculoCreditoPersonalDolar;
+  private JButton enviarCorreo;
+  private JButton crearPDF;
   private JButton salir;
   private JPanel panel;
 
@@ -37,128 +37,128 @@ public class ventana extends JFrame {
     this.add(panel);
     panel.setBackground(new Color(102, 255, 102));
 
-    boton1 = new JButton("Agregar Cliente");
-    boton1.setBounds(50, 50, 200, 50);
-    boton1.setBackground(Color.white);
-    boton1.setForeground(Color.black);
-    panel.add(boton1);
-    boton2 = new JButton("Agregar Producto");
-    boton2.setBounds(50, 150, 200, 50);
-    boton2.setBackground(Color.white);
-    boton2.setForeground(Color.black);
-    panel.add(boton2);
-    boton3 = new JButton("Agregar Proveedor");
-    boton3.setBounds(50, 250, 200, 50);
-    boton3.setBackground(Color.white);
-    boton3.setForeground(Color.black);
-    panel.add(boton3);
-    boton4 = new JButton("Agregar Factura");
-    boton4.setBounds(50, 350, 200, 50);
-    boton4.setBackground(Color.white);
-    boton4.setForeground(Color.black);
-    panel.add(boton4);
-    boton5 = new JButton("Agregar Detalle Factura");
-    boton5.setBounds(50, 450, 200, 50);
-    boton5.setBackground(Color.white);
-    boton5.setForeground(Color.black);
-    panel.add(boton5);
-    boton6 = new JButton("Agregar Producto a Proveedor");
-    boton6.setBounds(50, 550, 200, 50);
-    boton6.setBackground(Color.white);
-    boton6.setForeground(Color.black);
-    panel.add(boton6);
-    boton7 = new JButton("Agregar Producto a Cliente");
-    boton7.setBounds(50, 650, 200, 50);
-    boton7.setBackground(Color.white);
-    boton7.setForeground(Color.black);
-    panel.add(boton7);
-    boton8 = new JButton("Agregar Producto a Factura");
-    boton8.setBounds(300, 50, 200, 50);
-    boton8.setBackground(Color.white);
-    boton8.setForeground(Color.black);
-    panel.add(boton8);
-    boton9 = new JButton("Agregar Producto a Detalle Factura");
-    boton9.setBounds(300, 150, 200, 50);
-    boton9.setBackground(Color.white);
-    boton9.setForeground(Color.black);
-    panel.add(boton9);
-    boton10 = new JButton("Agregar Proveedor a Factura");
-    boton10.setBounds(300, 250, 200, 50);
-    boton10.setBackground(Color.white);
-    boton10.setForeground(Color.black);
-    panel.add(boton10);
-    boton11 = new JButton("Agregar Cliente a Factura");
-    boton11.setBounds(300, 350, 200, 50);
-    boton11.setBackground(Color.white);
-    boton11.setForeground(Color.black);
-    panel.add(boton11);
-    boton12 = new JButton("Agregar Detalle Factura a Factura");
-    boton12.setBounds(300, 450, 200, 50);
-    boton12.setBackground(Color.white);
-    boton12.setForeground(Color.black);
-    panel.add(boton12);
+    registrarSolicitante = new JButton("registrar solicitante");
+    registrarSolicitante.setBounds(50, 50, 200, 50);
+    registrarSolicitante.setBackground(Color.white);
+    registrarSolicitante.setForeground(Color.black);
+    panel.add(registrarSolicitante);
+    registrarSolicitudCredito = new JButton("registrar solicitud credito");
+    registrarSolicitudCredito.setBounds(50, 150, 200, 50);
+    registrarSolicitudCredito.setBackground(Color.white);
+    registrarSolicitudCredito.setForeground(Color.black);
+    panel.add(registrarSolicitudCredito);
+    calcularCreditoHipotecarioEnColon = new JButton("calcular credito hipotecario en colon");
+    calcularCreditoHipotecarioEnColon.setBounds(50, 250, 200, 50);
+    calcularCreditoHipotecarioEnColon.setBackground(Color.white);
+    calcularCreditoHipotecarioEnColon.setForeground(Color.black);
+    panel.add(calcularCreditoHipotecarioEnColon);
+    calcularCreditoHipotecarioDolar = new JButton("calcular credito hipotecario en dolar");
+    calcularCreditoHipotecarioDolar.setBounds(50, 350, 200, 50);
+    calcularCreditoHipotecarioDolar.setBackground(Color.white);
+    calcularCreditoHipotecarioDolar.setForeground(Color.black);
+    panel.add(calcularCreditoHipotecarioDolar);
+    calculoCreditoFiduciarioColon = new JButton("calcular credito fiduciario en colon");
+    calculoCreditoFiduciarioColon.setBounds(50, 450, 200, 50);
+    calculoCreditoFiduciarioColon.setBackground(Color.white);
+    calculoCreditoFiduciarioColon.setForeground(Color.black);
+    panel.add(calculoCreditoFiduciarioColon);
+    calculoCreditoFiduciarioDolar = new JButton("calcular credito fiduciario en dolar");
+    calculoCreditoFiduciarioDolar.setBounds(50, 550, 200, 50);
+    calculoCreditoFiduciarioDolar.setBackground(Color.white);
+    calculoCreditoFiduciarioDolar.setForeground(Color.black);
+    panel.add(calculoCreditoFiduciarioDolar);
+    calculoCreditoPrendarioColor = new JButton("calcular credito prendario en color");
+    calculoCreditoPrendarioColor.setBounds(50, 650, 200, 50);
+    calculoCreditoPrendarioColor.setBackground(Color.white);
+    calculoCreditoPrendarioColor.setForeground(Color.black);
+    panel.add(calculoCreditoPrendarioColor);
+    calculoCreditoPrendarioDolar = new JButton("calcular credito prendario en dolar");
+    calculoCreditoPrendarioDolar.setBounds(300, 50, 200, 50);
+    calculoCreditoPrendarioDolar.setBackground(Color.white);
+    calculoCreditoPrendarioDolar.setForeground(Color.black);
+    panel.add(calculoCreditoPrendarioDolar);
+    calculoCreditoPersonalDolar = new JButton("calcular credito personal en dolar");
+    calculoCreditoPersonalDolar.setBounds(300, 150, 200, 50);
+    calculoCreditoPersonalDolar.setBackground(Color.white);
+    calculoCreditoPersonalDolar.setForeground(Color.black);
+    panel.add(calculoCreditoPersonalDolar);
+    calculoCreditoPersonalColor = new JButton("");
+    calculoCreditoPersonalColor.setBounds(300, 250, 200, 50);
+    calculoCreditoPersonalColor.setBackground(Color.white);
+    calculoCreditoPersonalColor.setForeground(Color.black);
+    panel.add(calculoCreditoPersonalColor);
+    enviarCorreo = new JButton("");
+    enviarCorreo.setBounds(300, 350, 200, 50);
+    enviarCorreo.setBackground(Color.white);
+    enviarCorreo.setForeground(Color.black);
+    panel.add(enviarCorreo);
+    crearPDF = new JButton("");
+    crearPDF.setBounds(300, 450, 200, 50);
+    crearPDF.setBackground(Color.white);
+    crearPDF.setForeground(Color.black);
+    panel.add(crearPDF);
     salir = new JButton("Salir");
     salir.setBounds(300, 650, 200, 50);
     salir.setBackground(Color.white);
     salir.setForeground(Color.black);
     panel.add(salir);
 
-    boton1.addActionListener(new ActionListener() {
+    registrarSolicitante.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
 
       }
     });
-    boton2.addActionListener(new ActionListener() {
+    registrarSolicitudCredito.addActionListener(new ActionListener() {
+      public void actszionPerformed(ActionEvent evt) {
+
+      }
+    });
+    calcularCreditoHipotecarioEnColon.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
 
       }
     });
-    boton3.addActionListener(new ActionListener() {
+    calcularCreditoHipotecarioDolar.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
 
       }
     });
-    boton4.addActionListener(new ActionListener() {
+    calculoCreditoFiduciarioColon.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
 
       }
     });
-    boton5.addActionListener(new ActionListener() {
+    calculoCreditoFiduciarioDolar.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
 
       }
     });
-    boton6.addActionListener(new ActionListener() {
+    calculoCreditoPrendarioColor.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
 
       }
     });
-    boton7.addActionListener(new ActionListener() {
+    calculoCreditoPrendarioDolar.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
 
       }
     });
-    boton8.addActionListener(new ActionListener() {
+    calculoCreditoPersonalDolar.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
 
       }
     });
-    boton9.addActionListener(new ActionListener() {
+    calculoCreditoPersonalColor.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
 
       }
     });
-    boton10.addActionListener(new ActionListener() {
+    enviarCorreo.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
 
       }
     });
-    boton11.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent evt) {
-
-      }
-    });
-    boton12.addActionListener(new ActionListener() {
+    crearPDF.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent evt) {
 
       }
